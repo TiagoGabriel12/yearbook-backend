5c102b8e11e34648cbcd04883353512d50bf25c2
TiagoGabriel12 <tgsn@aluno.ifnmg.edu.br>
2026-08-04 17:03:01 +0000

    feat: configura CORS para o frontend em domínio separado

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from 'express';                // importa o Express
+import cors from 'cors';            
 import alunosRouter from './routes/alunos.js'; // importa o router de alunos <- NOVO
 import mensagensRouter from './routes/mensagens.js';
 import logger from './middlewares/logger.js';      // importa o middleware de log
-import mensagensRouter from './routes/mensagens.js';
 
 const app = express();      // cria a aplicação Express
 const PORT = 3000;          // porta do servidor
 
+app.use(cors());
 app.use(express.json());    // 1º — parseia JSON do body
 app.use(logger);            // 2º — registra log de cada requisição
 
